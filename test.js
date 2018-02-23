b978c1e23a647cb4cfc0a07114de79a339d37d77
asionius <814181242@qq.com>
2018-02-23 11:55:30 +0800

    delete unrelevant code

--- test.js
+++ test.js
@@ -1,19 +1,12 @@
 var rados = require('rados')
 var zlib = require('zlib')
 var io = require('io')
-var hs = require('hyperscan');
 var fs = require('fs');
 var cluster = new rados.Rados('ceph', 'client.admin', '/etc/ceph/ceph.conf');
 cluster.connect()
 
-var hsReg = hs.compile('117.100.187.110', "L");
-var eolReg = hs.compile('\n', "L");
 console.log('connect success...');
 
-//console.log(cluster.listPool());
-//cluster.createPool('auth');
-//console.log(cluster.listPool());
-//var ioctx = cluster.createIoCtx('test')
 var ioctx = cluster.createIoCtx('logs')
 var stream = ioctx.open('124.207.253.85/2017-08-28/04:28')
 
@@ -21,7 +14,6 @@ console.time('read');
 let buf = stream.read();
 console.timeEnd('read');
 console.log(buf.length);
-//stream.close();
 fs.writeFile('test.gz', buf);
 
 console.time('gunzip');
@@ -29,7 +21,7 @@ var buffer = zlib.gunzip(buf);
 console.timeEnd('gunzip');
 console.log(buffer.length);
 stream.rewind();
-//
+
 var ms = new io.MemoryStream();
 var gunzipStream = zlib.createGunzip(ms);
 console.time('copyTo');
